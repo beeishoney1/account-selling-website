14f7e28b57800a26e9e46a6d510d9ac44b78aa6f
BeeIsHoney <kyawsuhein4379@gmail.com>
2025-03-21 23:06:35 +0630

    Increase database connection timeout

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,23 @@
+const fs = require('fs');
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: 3306 // Optional, since FreeDB uses port 3306
+  port: process.env.DB_PORT || 3306,
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
 
-db.connect((err) => {
+connection.connect((err) => {
   if (err) {
-    console.error('Database Connection Error:', err);
+    console.error('Error connecting to the database:', err);
     return;
   }
   console.log('Connected to the database');
 });
 
-module.exports = db;
+module.exports = connection;
